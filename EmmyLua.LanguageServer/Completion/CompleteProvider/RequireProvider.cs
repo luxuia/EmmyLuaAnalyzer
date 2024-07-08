8b69b8b6a8407cb10d81494974587a9199a215d0
luxuia <luxuia@foxmail.com>
2024-07-08 13:57:52 +0800

    fix module complete

--- EmmyLua.LanguageServer/Completion/CompleteProvider/RequireProvider.cs
+++ EmmyLua.LanguageServer/Completion/CompleteProvider/RequireProvider.cs
@@ -1,5 +1,7 @@
 ﻿using EmmyLua.CodeAnalysis.Syntax.Node.SyntaxNodes;
+using EmmyLua.Configuration;
 using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using static EmmyLua.CodeAnalysis.Workspace.Module.ModuleManager;
 
 namespace EmmyLua.LanguageServer.Completion.CompleteProvider;
 
@@ -23,6 +25,7 @@ public class RequireProvider : ICompleteProviderBase
                 moduleBase = string.Join('.', parts[..^1]);
             }
 
+            var addedMap = new HashSet<string>();
             foreach (var moduleInfo in moduleInfos)
             {
                 var filterText = moduleInfo.Name;
@@ -30,7 +33,7 @@ public class RequireProvider : ICompleteProviderBase
                 {
                     filterText = $"{moduleBase}.{filterText}";
                 }
-                
+                addedMap.Add(filterText);
                 context.Add(new CompletionItem
                 {
                     Label = moduleInfo.Name,
@@ -41,6 +44,58 @@ public class RequireProvider : ICompleteProviderBase
                     Data = moduleInfo.DocumentId?.Id.ToString()
                 });
             }
+
+            var workspace = context.SemanticModel.Compilation.Workspace;
+            var rootpath = Path.Combine(workspace.MainWorkspace, string.Join(Path.DirectorySeparatorChar, parts[..^1]));
+            if (Directory.Exists(rootpath)) {
+                // 根据require延迟加载的情况, 文件夹下面的没有加载进module, 需要这里处理
+
+                var lastpart = parts[parts.Length - 1];
+                var files = Directory.GetFiles(rootpath, "*.lua");
+                foreach (var file in files) {
+
+                    var relatepath = file.Replace(workspace.MainWorkspace, "").Trim(Path.DirectorySeparatorChar);
+                    var name = relatepath.Replace(Path.DirectorySeparatorChar, '.').Replace(".lua", "");
+
+                    var suffix = name.Replace(moduleBase + ".", "");
+                    if (suffix.StartsWith(lastpart)) {
+
+                        if (!addedMap.Contains(name)) {
+                            addedMap.Add(name);
+                            context.Add(new CompletionItem {
+                                Label = suffix,
+                                Kind = CompletionItemKind.File,
+                                Detail = relatepath,
+                                FilterText = name,
+                                InsertText = name
+                            });
+                        }
+                    }
+                }
+
+                var dirs = Directory.GetDirectories(rootpath);
+                foreach (var file in dirs) {
+
+                    var relatepath = file.Replace(workspace.MainWorkspace, "").Trim(Path.DirectorySeparatorChar);
+                    var name = relatepath.Replace(Path.DirectorySeparatorChar, '.');
+
+                    var suffix = name.Replace(moduleBase + ".", "");
+                    if (suffix.StartsWith(lastpart)) {
+
+                        if (!addedMap.Contains(name)) {
+                            addedMap.Add(name);
+                            context.Add(new CompletionItem {
+                                Label = suffix,
+                                Kind = CompletionItemKind.Folder,
+                                Detail = relatepath,
+                                FilterText = name,
+                                InsertText = name
+                            });
+                        }
+                    }
+                }
+            }
+
             context.StopHere();
         }
     }
