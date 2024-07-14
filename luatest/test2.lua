1cf455aa448e33b7670e4ffe7d93c2d19c8563cd
luxuia <luxuia@foxmail.com>
2024-07-15 00:09:53 +0800

    test code

--- luatest/test2.lua
+++ luatest/test2.lua
@@ -1,4 +1,42 @@
-local test_define = require 'global'
+---@class Base
+local base = {desc = 'CLASSB'} 
+
+---类型A
+---@class A : Base
+local classA = {desc = 'CLASSA'}
 
 
-test_define.mg3.test
+---@param class A
+---@return A
+function test(class)
+end
+
+---@type A
+local a 
+
+---@type A[]
+local a_list
+
+
+---@type table<string, A>
+local a_tabla
+
+---@alias enuma interger | 1 | 2 | 3
+---@param a enuma
+function test_enum(a)
+end
+
+test_enum(1)
+
+
+local t = {}
+---@mapping new
+t:ctor(a, b)
+
+t:new()
+
+---@enum IO: number
+IO = {
+    Input = 1,
+    Output = '2',
+}
